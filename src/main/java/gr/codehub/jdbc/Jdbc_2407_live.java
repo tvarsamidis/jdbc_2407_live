b41948614bf27b7b9c7f7a3b8368e119b26b7b5a
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-07-26 09:41:08 +0300

    Init project

---
+++ src/main/java/gr/codehub/jdbc/Jdbc_2407_live.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.codehub.jdbc;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Jdbc_2407_live {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
