d0e49b525cff189bc76382ac1082f42e638e3692
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-07-26 10:51:57 +0300

    First JDBC connection

--- src/main/java/gr/codehub/jdbc/Jdbc_2407_live.java
+++ src/main/java/gr/codehub/jdbc/Jdbc_2407_live.java
@@ -1,16 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package gr.codehub.jdbc;
 
-/**
- *
- * @author Thomas
- */
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Jdbc_2407_live {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static void driverLoad() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
     }
+
+    private static Connection connectToMySql(String databaseName) throws SQLException {
+        String connectionUrl = "jdbc:mysql://localhost:3306";
+        String userName = "root";
+        String password = "root";
+        String database = databaseName;
+
+        Connection connection = null;
+        System.out.println("Connecting to database " + database);
+        connection = DriverManager.getConnection(connectionUrl + "/" + database, userName, password);
+        System.out.println("Connected!");
+        return connection;
+    }
+
+    
+    private static void testMetadata(Connection connection) throws SQLException {
+        DatabaseMetaData md = connection.getMetaData();
+        System.out.println("Database name = " + md.getDatabaseProductName());
+        System.out.println("Database version = " + md.getDatabaseProductVersion());
+    }
+
+    
+    private static void testAllTableColumns(Connection connection, String table) throws SQLException {
+        Statement statement = connection.createStatement();
+        String sql = """
+                     SELECT *
+                     FROM INFORMATION_SCHEMA.COLUMNS
+                     WHERE TABLE_NAME =
+                     """ ;
+        sql += "'" + table + "'";
+        ResultSet rs = statement.executeQuery(sql);
+        while(rs.next()) {
+            String s = rs.getString("column_name").toUpperCase();
+            String t = rs.getString("data_type");
+            System.out.println(s +" [" + t + "]");
+        }
+        rs.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Application started");
+        driverLoad();
+        Connection connection = connectToMySql("deletedemo");
+        testMetadata(connection);
+        testAllTableColumns(connection, "language");
+    }
+
 }
